0a9154ccc17fe9658e3f2ba730cc2cc95d0941d9
Rob Anderson <rob.anderson@ncl.ac.uk>
2021-09-17 15:18:28 +0100

    chore: add todo

--- src/pretalx/pretalx-structs.ts
+++ src/pretalx/pretalx-structs.ts
@@ -1,5 +1,7 @@
 import { array, number, object, string } from 'superstruct'
 
+// TODO: sync up with PretalxService's Config
+
 export const PretalxConfigStruct = object({
   eventSlug: string(),
   localeKeys: array(string()),
